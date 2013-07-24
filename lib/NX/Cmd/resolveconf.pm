7e01f24ab6fbf24d2e29ed7aac61bc89863c43b7
Graham Ollis <perl@wdlabs.com>
2013-07-24 11:50:30 -0400

    initial implementation

--- lib/NX/Cmd/resolveconf.pm
+++ lib/NX/Cmd/resolveconf.pm
@@ -2,9 +2,48 @@ package NX::Cmd::resolveconf;
 
 use strict;
 use warnings;
-use v5.10;
+use autodie;
+use 5.012;
+use File::Temp qw( tempfile );
+use IPC::Run qw( run );
+use Getopt::Long qw( GetOptions );
 
 # ABSTRACT: Jealously edit resolve.conf
 # VERSION
+
+sub main
+{
+  my $cmd = shift;
+  my @ARGV = @_;
+
+  my $editor = $ENV{EDITOR} // 'vi';
+
+  GetOptions(
+    'editor=s' => \$editor,
+  );
+
+  my($fh, $filename) = tempfile( "resolv-conf-XXXX", SUFFIX => '.conf', TMPDIR => 1 );
+
+  open my $in, '<', '/etc/resolv.conf';
+  my $old = do { local $/; <$in> };
+  print $fh $old;
+  close $in;
+  close $fh;
+
+  system $editor, $filename;
+
+  open $in, '<', $filename;
+  my $new = do { local $/; <$in> };
+  close $in;
+
+  if($new ne $old)
+  {
+    system qw( sudo chattr -i /etc/resolv.conf );
+    system 'sudo', 'cp', $filename, '/etc/resolv.conf';
+    system qw( sudo chattr +i /etc/resolv.conf );
+  }
+
+  unlink $filename;
+}
 
 1;
