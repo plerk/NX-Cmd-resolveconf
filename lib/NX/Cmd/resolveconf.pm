3bd7173801838c668fa6a153daf2f23d8c0e23ce
Graham Ollis <perl@wdlabs.com>
2013-07-26 07:12:02 -0400

    make an alias for correctly spelled version

--- lib/NX/Cmd/resolveconf.pm
+++ lib/NX/Cmd/resolveconf.pm
@@ -2,11 +2,7 @@ package NX::Cmd::resolveconf;
 
 use strict;
 use warnings;
-use autodie;
-use 5.012;
-use File::Temp qw( tempfile );
-use IPC::Run qw( run );
-use Getopt::Long qw( GetOptions );
+use NX::Cmd::Resolvconf;
 
 # ABSTRACT: Jealously edit resolve.conf
 # VERSION
@@ -15,35 +11,8 @@ sub main
 {
   my $cmd = shift;
   my @ARGV = @_;
-
-  my $editor = $ENV{EDITOR} // 'vi';
-
-  GetOptions(
-    'editor=s' => \$editor,
-  );
-
-  my($fh, $filename) = tempfile( "resolv-conf-XXXX", SUFFIX => '.conf', TMPDIR => 1 );
-
-  open my $in, '<', '/etc/resolv.conf';
-  my $old = do { local $/; <$in> };
-  print $fh $old;
-  close $in;
-  close $fh;
-
-  system $editor, $filename;
-
-  open $in, '<', $filename;
-  my $new = do { local $/; <$in> };
-  close $in;
-
-  if($new ne $old)
-  {
-    system qw( sudo chattr -i /etc/resolv.conf );
-    system 'sudo', 'cp', $filename, '/etc/resolv.conf';
-    system qw( sudo chattr +i /etc/resolv.conf );
-  }
-
-  unlink $filename;
+  say 'redirect nx resolveconf => nx resolvconf';
+  NX::Cmd::Resolvconf->main(@ARGV);
 }
 
 1;
